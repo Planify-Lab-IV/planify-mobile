fc6ac10de2911870e000910b454ad402618bde7a
luciabetelu <lubetelu@gmail.com>
2026-08-20 21:00:05 -0300

    creo cliente dio

---
+++ lib/core/network/dio_client.dart
@@ -0,0 +1,34 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+
+class DioClient {
+  DioClient._();
+
+  static Dio create({String? bearerToken}) {
+    final baseUrl = const String.fromEnvironment(
+      'API_BASE_URL',
+      defaultValue: 'http://localhost:8080',
+    );
+
+    final dio = Dio(BaseOptions(
+      baseUrl: baseUrl,
+      connectTimeout: const Duration(seconds: 10),
+      receiveTimeout: const Duration(seconds: 10),
+    ));
+
+    dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) {
+        if (bearerToken != null) {
+          options.headers['Authorization'] = 'Bearer $bearerToken';
+        }
+        handler.next(options);
+      },
+    ));
+
+    if (kDebugMode) {
+      dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
+    }
+
+    return dio;
+  }
+}
