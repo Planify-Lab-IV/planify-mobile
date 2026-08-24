7c840c0df8edd56b7d532891ca30801026bab8e0
luciabetelu <lubetelu@gmail.com>
2026-08-24 11:24:08 -0300

    creo providers para informacion global

---
+++ lib/core/providers/core_providers.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../data/dio_client.dart';
+import '../../data/secure_storage.dart';
+
+final secureStorageProvider = Provider<SecureStorage>((ref) {
+  return FlutterSecureStorageImpl();
+});
+
+final dioClientProvider = Provider<Dio>((ref) {
+  final storage = ref.watch(secureStorageProvider);
+  return DioClient.create(storage: storage);
+});
+
+class LocaleNotifier extends StateNotifier<Locale?> {
+  LocaleNotifier() : super(null);
+
+  void setLocale(Locale? locale) {
+    state = locale;
+  }
+}
+
+final localeNotifierProvider = StateNotifierProvider<LocaleNotifier, Locale?>((
+  ref,
+) {
+  return LocaleNotifier();
+});
