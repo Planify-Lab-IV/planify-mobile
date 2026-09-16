3064be11d5ef0c30fa96f85dbe800b17bc68c4a2
LucioZampella <lzluciozampella@gmail.com>
2026-09-16 20:39:49 -0300

    PLANIFY-143: Configuracion del cliente mobile para el backend aws

--- lib/data/dio_client.dart
+++ lib/data/dio_client.dart
@@ -16,9 +16,9 @@ class DioClient {
     final dio = Dio(
       BaseOptions(
         baseUrl: baseUrl,
-        connectTimeout: const Duration(seconds: 10),
-        receiveTimeout: const Duration(seconds: 10),
-        sendTimeout: const Duration(seconds: 10),
+        connectTimeout: const Duration(seconds: 20),
+        receiveTimeout: const Duration(seconds: 20),
+        sendTimeout: const Duration(seconds: 20),
         headers: const {
           'Accept': 'application/json',
           'Content-Type': 'application/json',
